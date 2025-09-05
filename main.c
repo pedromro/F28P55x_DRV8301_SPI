9f1ea2be805c854f970dc6a882f059c42afecd17
Pedro Oliveira <o.pedro@mphibr.com>
2025-09-05 06:05:33 +0530

    Trying to read.

--- main.c
+++ main.c
@@ -10,6 +10,22 @@ static void on_ctrl1_read(uint16_t raw, uint16_t data11, void *user)
     latest_ctrl1_data11 = data11;
 }
 
+static volatile uint16_t latest_status1_data11 = 0;
+static void on_status1_read(uint16_t raw, uint16_t data11, void *user)
+{
+    (void)raw; (void)user;
+    latest_status1_data11 = data11;
+}
+
+static volatile uint16_t latest_status2_data11 = 0;
+static void on_status2_read(uint16_t raw, uint16_t data11, void *user)
+{
+    (void)raw; (void)user;
+    latest_status2_data11 = data11;
+}
+
+
+
 int main(void)
 {
     Board_initClocksAndPIE();
@@ -18,12 +34,16 @@ int main(void)
     Board_initCPUTimer0_100ms();    // periodic Status1 reads
 
     // One-shot CONTROL1 read with callback
-    DRV8301_queueRead(DRV8301_REG_CONTROL1, on_ctrl1_read, 0);
+    // DRV8301_queueRead(DRV8301_REG_CONTROL1, on_ctrl1_read, 0);
+    // DRV8301_queueRead(DRV8301_REG_STATUS1, on_status1_read  , 0);
 
-    // Example write: set GATE_RESET (bit toggled high)
-    DRV8301_Ctrl1_t c1 = { .all = 0 };
-    c1.bits.GATE_RESET = 1;
-    DRV8301_queueWrite(DRV8301_REG_CONTROL1, c1.all, 0, 0);
+    DRV8301_queueRead(DRV8301_REG_STATUS2, on_status2_read  , 0);
+    
+
+    // // Example write: set GATE_RESET (bit toggled high)
+    // DRV8301_Ctrl1_t c1 = { .all = 0 };
+    // c1.bits.GATE_RESET = 1;
+    // DRV8301_queueWrite(DRV8301_REG_CONTROL1, c1.all, 0, 0);
 
     for (;;)
     {
